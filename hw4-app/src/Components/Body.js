726d4528a753da72783f9d1c27236d311954dc09
yiweizhangcs <yiwei_zhang@ucsb.edu>
2020-06-02 01:34:56 -0700

    hw7

---
+++ hw4-app/src/Components/Body.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Home from './Home'
+import Images from './Images'
+import Videos from './Videos'
+import Projects from './Projects'
+import GuestBook from './GuestBook'
+import MovieGallery from './MovieGallery'
+import AddMovie from './AddMovie'
+import CreateList from './CreateList'
+
+export class Body extends Component {
+  displayContent = () => {
+    var activeTab = this.props.activeTab
+    if(activeTab == 1){
+      return <Home/>;
+    }else if(activeTab == 2){
+      return <Images/>;
+    }else if(activeTab == 3){
+      return <Videos/>;
+    }else if(activeTab == 4){
+      return <Projects/>;
+    }else if(activeTab == 5){
+      return <GuestBook/>;
+    }else if(activeTab == 6){
+      return <MovieGallery/>;
+    }else if(activeTab == 7){
+      return <AddMovie/>;
+    }else{
+      return <CreateList/>;
+    }
+  }
+  render() {
+    return (this.displayContent());
+  }
+}
+export default Body;
