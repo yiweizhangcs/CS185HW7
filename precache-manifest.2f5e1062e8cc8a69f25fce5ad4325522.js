a66c328a195b423645f1d784bbe03b5fc648b062
yiweizhangcs <yiwei_zhang@ucsb.edu>
2020-06-02 01:42:44 -0700

    Updates

---
+++ precache-manifest.2f5e1062e8cc8a69f25fce5ad4325522.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5218e8243efbc1a05edabf4df7ebb8e0",
+    "url": "/CS185HW7/index.html"
+  },
+  {
+    "revision": "2cf878d741e504992b75",
+    "url": "/CS185HW7/static/css/main.1f87d354.chunk.css"
+  },
+  {
+    "revision": "04e539b5d1b24a346a88",
+    "url": "/CS185HW7/static/js/2.add8bd41.chunk.js"
+  },
+  {
+    "revision": "a17b95a8c249d5592103887f8cc9d740",
+    "url": "/CS185HW7/static/js/2.add8bd41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cf878d741e504992b75",
+    "url": "/CS185HW7/static/js/main.6ae8d8ce.chunk.js"
+  },
+  {
+    "revision": "1a39aa5f5891487e8ae9",
+    "url": "/CS185HW7/static/js/runtime-main.883c197b.js"
+  },
+  {
+    "revision": "cec961299effab0546e93d1896dab998",
+    "url": "/CS185HW7/static/media/HW26.cec96129.jpg"
+  },
+  {
+    "revision": "7dbb9c4af258d64c3c19a73693ef2942",
+    "url": "/CS185HW7/static/media/dog.7dbb9c4a.jpeg"
+  },
+  {
+    "revision": "20d8ff7ff17d130d2216fe5dc31279ac",
+    "url": "/CS185HW7/static/media/littledog.20d8ff7f.jpg"
+  },
+  {
+    "revision": "f7b6bcf60971c7ba24cfa7ee9a03827c",
+    "url": "/CS185HW7/static/media/mycat2.f7b6bcf6.jpg"
+  },
+  {
+    "revision": "08b1331279b161c15f19a9efa303ac4b",
+    "url": "/CS185HW7/static/media/planet.08b13312.jpg"
+  },
+  {
+    "revision": "0f5b6f6c9e6e7dc9c29306c523ae4814",
+    "url": "/CS185HW7/static/media/project1.0f5b6f6c.png"
+  },
+  {
+    "revision": "741bad24e1ad4f70d87a9a2bf9cc8763",
+    "url": "/CS185HW7/static/media/project2.741bad24.jpg"
+  },
+  {
+    "revision": "1562df8c78edc9ae9d6aff5649ff6295",
+    "url": "/CS185HW7/static/media/project3.1562df8c.jpg"
+  },
+  {
+    "revision": "6002e648cf5a45c8951b0304df07f4a8",
+    "url": "/CS185HW7/static/media/snow.6002e648.jpg"
+  },
+  {
+    "revision": "1dfed4b6943eff12af0219582ed8aa3a",
+    "url": "/CS185HW7/static/media/venus.1dfed4b6.jpg"
+  },
+  {
+    "revision": "36f415c734a13b47e366325beeb72918",
+    "url": "/CS185HW7/static/media/waterfall.36f415c7.jpg"
+  },
+  {
+    "revision": "9cc7a2e82271670fa62c0f3be8a5664e",
+    "url": "/CS185HW7/static/media/yasuo.9cc7a2e8.jpg"
+  }
+]);
